e0423a3b9b5c2548b07b38f2617d2344bb654364
prakash-ca <prakash.sahu@cognyx.ai>
2025-04-26 16:51:46 +0530

    merge with dsa

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 let data = [4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
-let newNumber = 102;
-let position = 3;
-data.splice(position, 0, newNumber, 103);
-// console.log(data);
 
-let newArray = data.slice(position, 5);
-// console.log(newArray);
+data.push(15);
+
+console.log(data);
